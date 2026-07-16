e221883f04fc2401fb78effd69638ffaa2cf492a
Yaroslav Nosenko <yaroslav.nosenko.mail@gmail.com>
2026-07-15 21:29:46 -0400

    add total

---
+++ drizzle/20260716012635_total/migration.sql
@@ -0,0 +1,12 @@
+-- Custom SQL migration file, put your code below! --
+CREATE VIEW total AS
+SELECT
+  chain,
+  token,
+  recipient,
+  SUM(amount::numeric)::text AS total
+FROM transactions
+GROUP BY
+  chain,
+  token,
+  recipient;
